8725b49661a494cb2fd0ca185045c2ea89962455
Claus-Justus Heine <Claus-Justus.Heine@IANS.Uni-Stuttgart.DE>
2020-11-03 21:36:38 +0100

    Fire an event if a translation is not found.

--- lib/private/L10N/L10N.php
+++ lib/private/L10N/L10N.php
@@ -208,6 +208,13 @@ class L10N implements IL10N {
 	}
 
 	/**
+	 * @return string The app this object was configured for
+	 */	
+	public function getAppName(): string {
+		return $this->app;
+	}
+	
+	/**
 	 * @param string $translationFile
 	 * @return bool
 	 */
