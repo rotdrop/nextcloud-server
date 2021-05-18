10ed239c470a7e40ec33fc503b0d4852008698b1
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-05-18 14:36:12 +0200

    IL10N & implementation: Export plain translations array.

--- lib/private/L10N/LazyL10N.php
+++ lib/private/L10N/LazyL10N.php
@@ -49,4 +49,8 @@ class LazyL10N implements IL10N {
 	public function getLocaleCode(): string {
 		return $this->getL()->getLocaleCode();
 	}
+
+	public function getTranslations(): array {
+		return $this->getL()->getTranslations();
+	}
 }
