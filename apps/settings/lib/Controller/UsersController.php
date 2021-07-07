2f26e3c5899048cff75f736b2ec7c5f0ecae0f50
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-07-07 14:47:44 +0200

    Re-enable counting of users for LDAP.

--- apps/settings/lib/Controller/UsersController.php
+++ apps/settings/lib/Controller/UsersController.php
@@ -112,14 +112,14 @@ class UsersController extends Controller {
 		if ($this->config->getSystemValueBool('sort_groups_by_name', false)) {
 			$sortGroupsBy = \OC\Group\MetaData::SORT_GROUPNAME;
 		} else {
-			if ($this->appManager->isEnabledForUser('user_ldap')) {
-				$isLDAPUsed =
-					$this->groupManager->isBackendUsed('\OCA\User_LDAP\Group_Proxy');
-				if ($isLDAPUsed) {
-					// LDAP user count can be slow, so we sort by group name here
-					$sortGroupsBy = \OC\Group\MetaData::SORT_GROUPNAME;
-				}
-			}
+			// if ($this->appManager->isEnabledForUser('user_ldap')) {
+			// 	$isLDAPUsed =
+			// 		$this->groupManager->isBackendUsed('\OCA\User_LDAP\Group_Proxy');
+			// 	if ($isLDAPUsed) {
+			// 		// LDAP user count can be slow, so we sort by group name here
+			// 		$sortGroupsBy = \OC\Group\MetaData::SORT_GROUPNAME;
+			// 	}
+			// }
 		}
 
 		$canChangePassword = $this->canAdminChangeUserPasswords();
@@ -143,11 +143,11 @@ class UsersController extends Controller {
 			'canRemove' => $adminGroup->canRemoveUser(),
 		];
 
-		if (!$isLDAPUsed && $this->appManager->isEnabledForUser('user_ldap')) {
-			$isLDAPUsed = (bool) array_reduce($this->userManager->getBackends(), function ($ldapFound, $backend) {
-				return $ldapFound || $backend instanceof User_Proxy;
-			});
-		}
+		// if (!$isLDAPUsed && $this->appManager->isEnabledForUser('user_ldap')) {
+		// 	$isLDAPUsed = (bool) array_reduce($this->userManager->getBackends(), function ($ldapFound, $backend) {
+		// 		return $ldapFound || $backend instanceof User_Proxy;
+		// 	});
+		// }
 
 		$disabledUsers = -1;
 		$userCount = 0;
