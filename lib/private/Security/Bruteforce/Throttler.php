1022c9b734dea98fdcb86c644fb4fbc6926653f4
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-07-24 10:13:49 +0200

    Disable Throttler for console login.

--- lib/private/Security/Bruteforce/Throttler.php
+++ lib/private/Security/Bruteforce/Throttler.php
@@ -287,6 +287,9 @@ class Throttler {
 	 * @param array $metadata
 	 */
 	public function resetDelay(string $ip, string $action, array $metadata): void {
+		if (empty($ip)) {
+			return;
+		}
 		$ipAddress = new IpAddress($ip);
 		if ($this->isIPWhitelisted((string)$ipAddress)) {
 			return;
