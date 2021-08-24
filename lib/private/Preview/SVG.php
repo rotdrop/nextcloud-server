2d42182018a699054e36a4643aca6f29a432a907
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-08-25 00:10:43 +0200

    SVG Preview: allow xlink:href for local anchors.

--- lib/private/Preview/SVG.php
+++ lib/private/Preview/SVG.php
@@ -31,7 +31,14 @@ class SVG extends ProviderV2 {
 
 			// Do not parse SVG files with references
 			if (preg_match('/["\s](xlink:)?href\s*=/i', $content)) {
-				return null;
+				if (preg_match_all('/xlink:href="([^"]*)"/', $content, $matches) > 0) {
+					foreach ($matches[1] as $link) {
+						if ($link[0] != '#') {
+							return null;
+						}
+					}
+					// \OCP\Util::writeLog('preview', print_r($matches, true), \OCP\Util::INFO);
+				}
 			}
 
 			$svg = new \Imagick();
