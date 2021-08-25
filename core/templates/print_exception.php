5c0bbd034a14d0e388d931d4b0ba32a93a16acc5
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-08-25 15:36:02 +0200

    Exception template: print trace if config verbose_exceptions is set, add CSS-classes to the exception template.
    The idea is to have a styleable exception template.

--- core/templates/print_exception.php
+++ core/templates/print_exception.php
@@ -6,31 +6,37 @@
  */
 
 function print_exception(Throwable $e, \OCP\IL10N $l): void {
-	print_unescaped('<pre>');
+	print_unescaped('<pre class="trace">');
 	p($e->getTraceAsString());
 	print_unescaped('</pre>');
 
 	$e = $e->getPrevious();
 	if ($e !== null) {
-		print_unescaped('<br />');
-		print_unescaped('<h4>');
+		print_unescaped('<div class="previous">');
+		print_unescaped('<h3 class="previous">');
 		p($l->t('Previous'));
-	    print_unescaped('</h4>');
-
-		print_unescaped('<ul>');
-		print_unescaped('<li>');
+		print_unescaped('</h3>');
+		print_unescaped('       <ul class="technical">');
+		print_unescaped('               <li class="class">');
+		p($l->t('Type: %s', get_class($e)));
+		print_unescaped('               </li>');
+		print_unescaped('               <li class="code">');
 		p($l->t('Code: %s', $e->getCode()));
-		print_unescaped('</li>');
-		print_unescaped('<li>');
+		print_unescaped('               </li>');
+		print_unescaped('               <li class="message">');
 		p($l->t('Message: %s', $e->getMessage()));
-		print_unescaped('</li>');
-		print_unescaped('<li>');
+		print_unescaped('               </li>');
+		print_unescaped('               <li class="file">');
 		p($l->t('File: %s', $e->getFile()));
-		print_unescaped('</li>');
-		print_unescaped('<li>');
+		print_unescaped('               </li>');
+		print_unescaped('               <li class="line">');
 		p($l->t('Line: %s', $e->getLine()));
-		print_unescaped('</li>');
-		print_unescaped('</ul>');
+		print_unescaped('               </li>');
+		print_unescaped('       </ul>');
+		print_unescaped('   <h3 class="trace">');
+		p($l->t('Trace'));
+		print_unescaped('       </h3>');
 		print_exception($e, $l);
+		print_unescaped('</div>'); // close previous
 	}
 }
