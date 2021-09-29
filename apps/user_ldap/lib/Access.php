7ad7e6df1f5f286c1de30153ac8d351f16195b48
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-09-29 15:08:01 +0200

    Danger Zone: optionally force the configured LDAP uid even if the user already exists.

--- apps/user_ldap/lib/Access.php
+++ apps/user_ldap/lib/Access.php
@@ -582,8 +582,10 @@ class Access extends LDAPUtility {
 				// setups, and most importantly: this is not intended.
 				return false;
 			}
+			$intNameForce = (bool)$this->connection->ldapExpertUsernameForce;
 		} else {
 			$intName = $this->sanitizeGroupIDCandidate($ldapName);
+			$intNameForce = false;
 		}
 
 		//a new user/group! Add it only if it doesn't conflict with other backend's users or existing groups
@@ -593,7 +595,8 @@ class Access extends LDAPUtility {
 		$originalTTL = $this->connection->ldapCacheTTL;
 		$this->connection->setConfiguration(['ldapCacheTTL' => 0]);
 		if ($intName !== ''
-			&& (($isUser && !$this->ncUserManager->userExists($intName))
+			&& ($intNameForce
+				|| ($isUser && !$this->ncUserManager->userExists($intName))
 				|| (!$isUser && !\OC::$server->getGroupManager()->groupExists($intName))
 			)
 		) {
