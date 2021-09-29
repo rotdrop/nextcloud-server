628915dfdb844a62b7f91e9c5914b36862a3fb33
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-09-29 16:09:41 +0200

    Fix toast-not-shown by including toast-styles.

--- apps/settings/src/store/index.js
+++ apps/settings/src/store/index.js
@@ -29,6 +29,8 @@ import settings from './settings'
 import oc from './oc'
 import { showError } from '@nextcloud/dialogs'
 
+require('@nextcloud/dialogs/styles/toast.scss')
+
 Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production'
