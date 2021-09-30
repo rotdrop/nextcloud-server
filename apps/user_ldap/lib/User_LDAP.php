b0dd9f2cf73fc67340f8b5e5249551644c870ea6
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-09-30 18:04:58 +0200

    Fix userExists() to really check if it exists in LDAP.
    User_LDAP::userExists() just returned true if the users exists at all,
    which is wrong. It should only return true if the user exists on LDAP (+
    cache logic of course), but not return true if it exists in any other
    user backend, as it did before.

--- apps/user_ldap/lib/User_LDAP.php
+++ apps/user_ldap/lib/User_LDAP.php
@@ -342,8 +342,12 @@ class User_LDAP extends BackendUtility implements IUserBackend, UserInterface, I
 			return false;
 		}
 
-		$this->access->connection->writeToCache('userExists' . $uid, true);
-		return true;
+		if ($this->userExistsOnLDAP($uid)) {
+			$this->access->connection->writeToCache('userExists' . $uid, true);
+			return true;
+		}
+
+		return false;
 	}
 
 	/**
