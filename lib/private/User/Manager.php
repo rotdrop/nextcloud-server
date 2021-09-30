7d4ded5f806b0afb07842b67a978597d3662accb
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-09-30 08:40:02 +0200

    Allow creating of existing user in an other backend.
    Idea is to have some sort of migration of users to another backend.
    However, in principle to have this the user-backend should be made a
    config option to backends which support creation of users.

--- lib/private/User/Manager.php
+++ lib/private/User/Manager.php
@@ -384,6 +384,15 @@ class Manager extends PublicEmitter implements IUserManager {
 		$assertion = \OC::$server->get(IAssertion::class);
 		$assertion->createUserIsLegit();
 
+		if (!$this->verifyUid($uid)) {
+			throw new \InvalidArgumentException($l->t('Username is invalid because files already exist for this user'));
+		}
+
+		// Check if user already exists
+		if ($this->userExists($uid)) {
+			throw new \InvalidArgumentException($l->t('The username is already being used'));
+		}
+
 		$localBackends = [];
 		foreach ($this->backends as $backend) {
 			if ($backend instanceof Database) {
@@ -438,18 +447,14 @@ class Manager extends PublicEmitter implements IUserManager {
 			throw new \InvalidArgumentException($l->t('Username must not consist of dots only'));
 		}
 
-		if (!$this->verifyUid($uid)) {
-			throw new \InvalidArgumentException($l->t('Username is invalid because files already exist for this user'));
+		// Check if user already exists in this backend
+		if ($backend->userExists($uid)) {
+			throw new \InvalidArgumentException($l->t('The username is already being used'));
 		}
 
 		// No empty password
 		if (trim($password) === '') {
 			throw new \InvalidArgumentException($l->t('A valid password must be provided'));
-		}
-
-		// Check if user already exists
-		if ($this->userExists($uid)) {
-			throw new \InvalidArgumentException($l->t('The username is already being used'));
 		}
 
 		/** @deprecated 21.0.0 use BeforeUserCreatedEvent event with the IEventDispatcher instead */
