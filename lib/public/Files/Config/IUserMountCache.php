b7663e79a0339fd4e20faeec39f3f5b50320ccaa
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-01-11 08:48:10 +0100

    Avoid excessive mount cache shootdown for authenticated and non-shareable mounts.
    This commit adds two new mount-flags 'authenticated' and
    'enable_sharing' which are used to avoid a mount-cache shoot-down for
    file-systems which by design cannot be shared of need authentication.
    
    This affects the way UserMountCache::registerMounts() cleans up the
    mount-cache:
    
    - non-shareable mounts are left in the cache if the mounts are
      registered for another user the currently logged on user
    - authenticated mounts are left in the cache if credentials are not
      available.
    
    The use case are special authenticated and non-shareable virtual
    file-systems.

--- lib/public/Files/Config/IUserMountCache.php
+++ lib/public/Files/Config/IUserMountCache.php
@@ -24,18 +24,21 @@ interface IUserMountCache {
 	 * @param IUser $user
 	 * @param IMountPoint[] $mounts
 	 * @param array|null $mountProviderClasses
+	 * @param null|string $forUser
 	 * @since 9.0.0
 	 */
-	public function registerMounts(IUser $user, array $mounts, ?array $mountProviderClasses = null);
+	public function registerMounts(IUser $user, array $mounts, ?array $mountProviderClasses = null, ?string $forUser = null);
 
 	/**
 	 * Get all cached mounts for a user
 	 *
 	 * @param IUser $user
+	 * @param null|bool $sharable
+	 * @param null|bool $authenticated
 	 * @return ICachedMountInfo[]
 	 * @since 9.0.0
 	 */
-	public function getMountsForUser(IUser $user);
+	public function getMountsForUser(IUser $user, ?bool $shareable = null, ?bool $authenticated = null);
 
 	/**
 	 * Get all cached mounts by storage
