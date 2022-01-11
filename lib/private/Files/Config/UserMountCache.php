69746242a8d5c61bbab9e585ee812854d8034cbf
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-01-11 08:48:10 +0100

    Files\UserMountCache: refuse to remove cached mounts if login-credentials are not available ...
    ... in my case I have mounts which are only available if the
    login-credentials are available. However, the new Collabora office
    integration connects to its (potentially shared?) file without
    login-credentials via a public page. It then sets the owner-user-id of
    the editited file in to the session and initializes the FS without
    login-credentials (it was a public page .... so no user-session etc.).

--- lib/private/Files/Config/UserMountCache.php
+++ lib/private/Files/Config/UserMountCache.php
@@ -126,10 +126,17 @@ class UserMountCache implements IUserMountCache {
 			/** @psalm-suppress InvalidArgument */
 			$this->mountsForUsers[$user->getUID()][] = $mount;
 		}
-		foreach ($removedMounts as $mount) {
-			$this->removeFromCache($mount);
-			$index = array_search($mount, $this->mountsForUsers[$user->getUID()]);
-			unset($this->mountsForUsers[$user->getUID()][$index]);
+		try {
+			/** @var \OCP\Authentication\LoginCredentials\IStore $credentialsStore */
+			$credentialsStore = \OC::$server->get(\OCP\Authentication\LoginCredentials\IStore::class);
+			$credentialsStore->getLoginCredentials();
+			foreach ($removedMounts as $mount) {
+				$this->removeFromCache($mount);
+				$index = array_search($mount, $this->mountsForUsers[$user->getUID()]);
+				unset($this->mountsForUsers[$user->getUID()][$index]);
+			}
+		} catch (\OCP\Authentication\Exceptions\CredentialsUnavailableException $t) {
+			$this->logger->logException($t);
 		}
 		foreach ($changedMounts as $mount) {
 			$this->updateCachedMount($mount);
