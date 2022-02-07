7b4cbcd6a3e853453d24b19744f53733b239cd83
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-02-07 16:46:22 +0100

    Prepare for adding groups with specified backend.

--- apps/provisioning_api/lib/Controller/GroupsController.php
+++ apps/provisioning_api/lib/Controller/GroupsController.php
@@ -110,6 +110,7 @@ class GroupsController extends AUserDataOCSController {
 				'disabled' => $group->countDisabled(),
 				'canAdd' => $group->canAddUser(),
 				'canRemove' => $group->canRemoveUser(),
+				'backends' => $group->getBackendNames(),
 			];
 		}, $groups));
 
@@ -247,7 +248,11 @@ class GroupsController extends AUserDataOCSController {
 	 */
 	#[AuthorizedAdminSetting(settings:Users::class)]
 	#[PasswordConfirmationRequired]
-	public function addGroup(string $groupid, string $displayname = ''): DataResponse {
+	public function addGroup(
+		string $groupid,
+		string $displayname = '',
+		string $backendName = ''
+	): DataResponse {
 		// Validate name
 		if (empty($groupid)) {
 			$this->logger->error('Group name not supplied', ['app' => 'provisioning_api']);
@@ -257,7 +262,24 @@ class GroupsController extends AUserDataOCSController {
 		if ($this->groupManager->groupExists($groupid)) {
 			throw new OCSException('group exists', 102);
 		}
-		$group = $this->groupManager->createGroup($groupid);
+		if (!empty($backendName)) {
+			$groupInterface = null;
+			foreach ($this->groupManager->getBackends() as $oneBackend) {
+				if ($oneBackend->getBackendName() == $backendName) {
+					$groupInterface = $oneBackend;
+				}
+			}
+			if (empty($groupInterface)) {
+				$errorString = 'Requested group-backend "' . $backendName . '" does not exist';
+				$this->logger->error('Failed addGroup attempt: ' . $errorString . '.', ['app' => 'ocs_api']);
+				throw new OCSException($errorString, 102);
+			}
+		}
+		if (empty($groupInterface)) {
+			$group = $this->groupManager->createGroup($groupid);
+		} else {
+			$group = $this->groupManager->createGroupFromBackend($groupid, $groupInterface);
+		}
 		if ($group === null) {
 			throw new OCSException('Not supported by backend', 103);
 		}
