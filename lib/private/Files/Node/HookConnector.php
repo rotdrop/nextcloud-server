58bfb791120595e96a8377db8052e3f36af478a5
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-07-15 12:03:44 +0200

    Files\Node\HookConnector: fix node type for non-existing rename and copy sources/targets.
    Currently, HookConnector::getNodeForPath() just asks
    Filesystem::is_dir($path) if a FileInfo for $path cannot be found.
    However, if $path is a non-existing target in the pre-rename or pre-copy
    events, or the then no-longer existing source in the post-rename event
    this will fail: Filesystem::is_dir($path) will return false (the $path
    does not event exist), and the a NonExistingFile node is generated in
    any case.
    
    This is at least surprising: when renaming an existing folder to
    non-existing target I really would expect a NonExistingFolder node as
    target in the pre-rename event, and the same for the source in the
    post-rename event.

--- lib/private/Files/Node/HookConnector.php
+++ lib/private/Files/Node/HookConnector.php
@@ -176,7 +176,7 @@ class HookConnector {
 
 	public function rename($arguments) {
 		$source = $this->getNodeForPath($arguments['oldpath']);
-		$target = $this->getNodeForPath($arguments['newpath']);
+		$target = $this->getNodeForPath($arguments['newpath'], $source);
 		$this->root->emit('\OC\Files', 'preRename', [$source, $target]);
 		$this->legacyDispatcher->dispatch('\OCP\Files::preRename', new GenericEvent([$source, $target]));
 
@@ -185,8 +185,8 @@ class HookConnector {
 	}
 
 	public function postRename($arguments) {
-		$source = $this->getNodeForPath($arguments['oldpath']);
 		$target = $this->getNodeForPath($arguments['newpath']);
+		$source = $this->getNodeForPath($arguments['oldpath'], $target);
 		$this->root->emit('\OC\Files', 'postRename', [$source, $target]);
 		$this->legacyDispatcher->dispatch('\OCP\Files::postRename', new GenericEvent([$source, $target]));
 
@@ -196,7 +196,7 @@ class HookConnector {
 
 	public function copy($arguments) {
 		$source = $this->getNodeForPath($arguments['oldpath']);
-		$target = $this->getNodeForPath($arguments['newpath']);
+		$target = $this->getNodeForPath($arguments['newpath'], $source);
 		$this->root->emit('\OC\Files', 'preCopy', [$source, $target]);
 		$this->legacyDispatcher->dispatch('\OCP\Files::preCopy', new GenericEvent([$source, $target]));
 
@@ -223,7 +223,7 @@ class HookConnector {
 		$this->dispatcher->dispatchTyped($event);
 	}
 
-	private function getNodeForPath($path) {
+	private function getNodeForPath($path, $relatedNode = null) {
 		$info = Filesystem::getView()->getFileInfo($path);
 		if (!$info) {
 			$fullPath = Filesystem::getView()->getAbsolutePath($path);
@@ -232,7 +232,8 @@ class HookConnector {
 			} else {
 				$info = null;
 			}
-			if (Filesystem::is_dir($path)) {
+			if (Filesystem::is_dir($path)
+				|| ($info === null && !empty($relatedNode) && ($relatedNode instanceof \OCP\Files\Folder))) {
 				return new NonExistingFolder($this->root, $this->view, $fullPath, $info);
 			} else {
 				return new NonExistingFile($this->root, $this->view, $fullPath, $info);
