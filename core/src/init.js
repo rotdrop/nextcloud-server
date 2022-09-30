42cb64fc4caee6614e19d901b1dbbb79b1afbc2b
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-09-30 14:53:09 +0200

    Fix "animating" snapper protection, it was broken.

--- core/src/init.js
+++ core/src/init.js
@@ -166,6 +166,11 @@ export const initCore = () => {
 			if (animating || snapper.state().state !== 'closed') {
 				return
 			}
+			// animating MUST be set here because of snapper
+			// open/close is deferred, so there may come any event in
+			// between before finally oldSnapperClose() is called. So
+			// animating really has to be set here now.
+			animating = true
 			oldSnapperOpen(SNAPPER_OPEN)
 		}
 
@@ -173,6 +178,11 @@ export const initCore = () => {
 			if (animating || snapper.state().state === 'closed') {
 				return
 			}
+			// animating MUST be set here because of snapper
+			// open/close is deferred, so there may come any event in
+			// between before finally oldSnapperClose() is called. So
+			// animating really has to be set here now.
+			animating = true
 			oldSnapperClose()
 		}
 
