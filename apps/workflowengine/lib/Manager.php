59fbb93ea77e85ca0ecf38aa31acc0ea27f1c16b
Claus-Justus Heine <himself@claus-justus-heine.de>
2023-08-24 15:10:04 +0200

    Workflowengine: add a new FilePath check which matches the complete path, including the mountpoint.
    Signed-off-by: Claus-Justus Heine <himself@claus-justus-heine.de>

--- apps/workflowengine/lib/Manager.php
+++ apps/workflowengine/lib/Manager.php
@@ -11,6 +11,7 @@ use NCU\WorkflowEngine\RuntimeOperation;
 use NCU\WorkflowEngine\RuntimeScope;
 use OCA\WorkflowEngine\Check\FileMimeType;
 use OCA\WorkflowEngine\Check\FileName;
+use OCA\WorkflowEngine\Check\FilePath;
 use OCA\WorkflowEngine\Check\FileSize;
 use OCA\WorkflowEngine\Check\FileSystemTags;
 use OCA\WorkflowEngine\Check\RequestRemoteAddress;
@@ -912,6 +913,7 @@ class Manager implements IManager {
 			return [
 				$this->container->get(FileMimeType::class),
 				$this->container->get(FileName::class),
+				$this->container->get(FilePath::class),
 				$this->container->get(FileSize::class),
 				$this->container->get(FileSystemTags::class),
 				$this->container->get(RequestRemoteAddress::class),
