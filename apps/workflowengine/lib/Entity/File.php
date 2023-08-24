77ad8a115182aa7c0759e7517c5ca8f3b54fbdc3
Claus-Justus Heine <himself@claus-justus-heine.de>
2023-08-25 01:13:08 +0200

    Workflowengine: listen to cache events.
    The use case are external storages like smb folder used to upload
    scanner data. Essentially, a cache miss is here just equivalent to a
    "file created", deleted, updated event. But those events do not fire in
    this case.

--- apps/workflowengine/lib/Entity/File.php
+++ apps/workflowengine/lib/Entity/File.php
@@ -63,6 +63,9 @@ class File implements IEntity, IDisplayText, IUrl, IIcon, IContextPortation {
 	#[\Override]
 	public function getEvents(): array {
 		return [
+			new GenericEntityEvent($this->l10n->t('File added to cache'), self::EVENT_NAMESPACE . 'addToCache'),
+			new GenericEntityEvent($this->l10n->t('File removed from cache'), self::EVENT_NAMESPACE . 'removeFromCache'),
+			new GenericEntityEvent($this->l10n->t('File cache updated'), self::EVENT_NAMESPACE . 'updateCache'),
 			new GenericEntityEvent($this->l10n->t('File created'), self::EVENT_NAMESPACE . 'postCreate'),
 			new GenericEntityEvent($this->l10n->t('File updated'), self::EVENT_NAMESPACE . 'postWrite'),
 			new GenericEntityEvent($this->l10n->t('File renamed'), self::EVENT_NAMESPACE . 'postRename'),
@@ -133,6 +136,9 @@ class File implements IEntity, IDisplayText, IUrl, IIcon, IContextPortation {
 			case self::EVENT_NAMESPACE . 'postWrite':
 			case self::EVENT_NAMESPACE . 'postDelete':
 			case self::EVENT_NAMESPACE . 'postTouch':
+			case self::EVENT_NAMESPACE . 'addToCache':
+			case self::EVENT_NAMESPACE . 'removeFromCache':
+			case self::EVENT_NAMESPACE . 'updateCache':
 				return $this->event->getSubject();
 			case self::EVENT_NAMESPACE . 'postRename':
 			case self::EVENT_NAMESPACE . 'postCopy':
