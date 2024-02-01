038abb2fc14742dd979f72ed19e5ac91462301e1
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-02-01 21:18:23 +0100

    Provisioning API, allow sub-admins to retrieve details about their groups.

--- apps/provisioning_api/lib/Controller/GroupsController.php
+++ apps/provisioning_api/lib/Controller/GroupsController.php
@@ -97,20 +97,30 @@ class GroupsController extends AUserData {
 	 * 200: Groups details returned
 	 */
 	#[NoAdminRequired]
-	#[AuthorizedAdminSetting(settings: Sharing::class)]
+	// #[AuthorizedAdminSetting(settings: Sharing::class)]
 	public function getGroupsDetails(string $search = '', ?int $limit = null, int $offset = 0): DataResponse {
+		$user = $this->userSession->getUser();
+		$isAdmin = $this->groupManager->isAdmin($user->getUID());
 		$groups = $this->groupManager->search($search, $limit, $offset);
-		$groups = array_map(function ($group) {
-			/** @var IGroup $group */
-			return [
-				'id' => $group->getGID(),
-				'displayname' => $group->getDisplayName(),
-				'usercount' => $group->count(),
-				'disabled' => $group->countDisabled(),
-				'canAdd' => $group->canAddUser(),
-				'canRemove' => $group->canRemoveUser(),
-				'backends' => $group->getBackendNames(),
-			];
+		$groups = array_map(function ($group) use ($user, $isAdmin) {
+
+			$isSubadminOfGroup = $this->groupManager->getSubAdmin()->isSubAdminOfGroup($user, $group);
+
+			// Check subadmin has access to this group
+			if ($isAdmin || $isSubadminOfGroup) {
+				/** @var IGroup $group */
+				return [
+					'id' => $group->getGID(),
+					'displayname' => $group->getDisplayName(),
+					'usercount' => $group->count(),
+					'disabled' => $group->countDisabled(),
+					'canAdd' => $group->canAddUser(),
+					'canRemove' => $group->canRemoveUser(),
+					'backends' => $group->getBackendNames(),
+				];
+			} else {
+				return [];
+			}
 		}, $groups);
 
 		return new DataResponse(['groups' => $groups]);
