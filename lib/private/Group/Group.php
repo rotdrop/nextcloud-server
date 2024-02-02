932211c4d00f80c949104cc85df091fd3de5e0d7
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-02-02 21:48:57 +0100

    Disable graceful user adding to groups in order to test event-scheme reimplentation.

--- lib/private/Group/Group.php
+++ lib/private/Group/Group.php
@@ -186,7 +186,7 @@ class Group implements IGroup {
 				try {
 					$backend->addToGroup($user->getUID(), $this->gid);
 				} catch (\Throwable $t) {
-					continue;
+					throw new \Exception('Backend cannot add user exception ' . get_class($backend) . ' USER ' . $user->getUID(), 0, $t);
 				}
 				if ($this->users) {
 					$this->users[$user->getUID()] = $user;
