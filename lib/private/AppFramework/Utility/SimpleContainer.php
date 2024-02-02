rotdrop/server

Include the original exception

lib/private/AppFramework/Utility/SimpleContainer.php
@@ -112,7 +112,7 @@ class SimpleContainer implements ArrayAccess, ContainerInterface, IContainer {
 					' Class can not be instantiated');
 			}
 		} catch (ReflectionException $e) {
-			throw new QueryException($baseMsg . ' ' . $e->getMessage());
+			throw new QueryException($baseMsg . ' ' . $e->getMessage(), $e->getCode(), $e);
 		}
 	}
 

rotdrop/nextcloud-server

lib/private/AppFramework/Utility/SimpleContainer.php
@@ -132,7 +132,7 @@ class SimpleContainer implements ArrayAccess, ContainerInterface, IContainer {
 			}
 		} catch (ReflectionException $e) {
 			// Class does not exist
-			throw new QueryNotFoundException($baseMsg . ' ' . $e->getMessage());
+			throw new QueryNotFoundException($baseMsg . ' ' . $e->getMessage(), $e->getCode(), $e);
 		}
 	}
 