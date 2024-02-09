b7546297072c0e493d4944e162225b1048b6f200
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-02-09 12:04:02 +0100

    BackgroundJob\JobList: log exceptions from individual jobs.

--- lib/private/BackgroundJob/JobList.php
+++ lib/private/BackgroundJob/JobList.php
@@ -316,6 +316,7 @@ class JobList implements IJobList {
 				/** @var IJob $job */
 				$job = \OCP\Server::get($row['class']);
 			} catch (ContainerExceptionInterface $e) {
+				$this->logger->info('ERROR', [ 'exception' => $e ]);
 				if (class_exists($row['class'])) {
 					$class = $row['class'];
 					$job = new $class();
