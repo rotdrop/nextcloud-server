f0ffa2b382a161cb0fde84023f0f7d2374300708
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-03-13 14:02:11 +0100

    apps/files Sidebar, silence some JS errors.

--- apps/files/src/views/Sidebar.vue
+++ apps/files/src/views/Sidebar.vue
@@ -39,7 +39,7 @@
 				:name="t('files', 'Favorite')"
 				inline />
 			{{ size }}
-			<NcDateTime :timestamp="fileInfo.mtime" />
+			<NcDateTime v-if="fileInfo" :timestamp="fileInfo.mtime" />
 		</template>
 
 		<!-- TODO: create a standard to allow multiple elements here? -->
@@ -491,7 +491,7 @@ export default {
 
 				await this.$nextTick()
 
-				if (focusTabAfterLoad) {
+				if (focusTabAfterLoad && this.$refs.sidebar) {
 					this.$refs.sidebar.focusActiveTabContent()
 				}
 			} catch (error) {
