dc71957148970b0116b2522a49cfdeda6854a305
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-05-26 12:01:19 +0200

    Update composer files after adding new files to lib.
    Caveat: the changes are numerous. However, I have looked at the composer
    git-repository and at the git blame for certain lines. I appears that
    Nextcloud uses a very special non-standard composer hacked version. For
    instance, "git blame" on the certain documentation lines in
    ClassLoader.php reveal that there was no documentation of class members
    and function parameters. It was introduced in commiti "Add type info to
    Autoload namespace" back in 2021 and never changed. Still the lines
    differ from what is used in Nextcloud (Debian special composer version
    perhaps?).

--- lib/composer/composer/installed.php
+++ lib/composer/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '4ff660ca2e0baa02440ba07296ed7e75fa544c0e',
+        'reference' => '8050115ef62a53f49372fb5e0d161a4b32cdd6f1',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '4ff660ca2e0baa02440ba07296ed7e75fa544c0e',
+            'reference' => '8050115ef62a53f49372fb5e0d161a4b32cdd6f1',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../../',
             'aliases' => array(),
