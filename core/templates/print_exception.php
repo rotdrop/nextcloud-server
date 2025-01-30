96e64ba15e1069e22a3105d2785f2ee62d93d205
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-01-30 09:38:31 +0100

    Core, exception templates: print also the messages from chained exceptions.

--- core/templates/print_exception.php
+++ core/templates/print_exception.php
@@ -9,7 +9,26 @@ use OCP\IL10N;
  * SPDX-FileCopyrightText: 2012-2015 ownCloud, Inc.
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+
 function print_exception(Throwable $e, IL10N $l): void {
+	print_unescaped('<h3>');
+	p($l->t('Details'));
+	print_unescaped('</h3>');
+	print_unescaped('<ul><li>');
+	p($l->t('Type: %s', [get_class($e)]));
+	print_unescaped('</li><li>');
+	p($l->t('Code: %s', [$e->getCode()]));
+	print_unescaped('</li><li>');
+	p($l->t('Message: %s', [$e->getMessage()]));
+	print_unescaped('</li><li>');
+	p($l->t('File: %s', [$e->getFile()]));
+	print_unescaped('</li><li>');
+	p($l->t('Line: %s', [$e->getLine()]));
+	print_unescaped('</li></ul>');
+	print_unescaped('<br />');
+	print_unescaped('<h3>');
+	p($l->t('Trace'));
+	print_unescaped('</h3>');
 	print_unescaped('<pre>');
 	p($e->getTraceAsString());
 	print_unescaped('</pre>');
