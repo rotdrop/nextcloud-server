99c3916fdf21e9fdc957c3a406f0cb411284dcfb
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-01-30 09:38:31 +0100

    Core, exception templates: print also the messages from chained exceptions.

--- core/templates/exception.php
+++ core/templates/exception.php
@@ -25,18 +25,9 @@ require_once __DIR__ . '/print_exception.php';
 	<ul>
 		<li><?php p($l->t('Remote Address: %s', [$_['remoteAddr']])) ?></li>
 		<li><?php p($l->t('Request ID: %s', [$_['requestID']])) ?></li>
-		<?php if (isset($_['debugMode']) && $_['debugMode'] === true): ?>
-			<li><?php p($l->t('Type: %s', [$_['errorClass']])) ?></li>
-			<li><?php p($l->t('Code: %s', [$_['errorCode']])) ?></li>
-			<li><?php p($l->t('Message: %s', [$_['errorMsg']])) ?></li>
-			<li><?php p($l->t('File: %s', [$_['file']])) ?></li>
-			<li><?php p($l->t('Line: %s', [$_['line']])) ?></li>
-		<?php endif; ?>
 	</ul>
 
 	<?php if (isset($_['debugMode']) && $_['debugMode'] === true): ?>
-		<br />
-		<h3><?php p($l->t('Trace')) ?></h3>
 		<?php print_exception($_['exception'], $l); ?>
 	<?php endif; ?>
 </div>
