2827a141a5425c6569cbb8347a003d045cd8b868
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-04-03 10:57:40 +0200

    2FA Provider loader: also log the original exception, because ...
    ... otherwise there is zarro chance to find the error.

--- lib/private/Authentication/TwoFactorAuth/ProviderLoader.php
+++ lib/private/Authentication/TwoFactorAuth/ProviderLoader.php
@@ -46,7 +46,7 @@ class ProviderLoader {
 						$providers[$provider->getId()] = $provider;
 					} catch (QueryException $exc) {
 						// Provider class can not be resolved
-						throw new Exception("Could not load two-factor auth provider $class");
+						throw new Exception("Could not load two-factor auth provider $class", 0, $exc);
 					}
 				}
 			}
@@ -60,7 +60,7 @@ class ProviderLoader {
 				$providers[$providerInstance->getId()] = $providerInstance;
 			} catch (QueryException $exc) {
 				// Provider class can not be resolved
-				throw new Exception('Could not load two-factor auth provider ' . $provider->getService());
+				throw new Exception('Could not load two-factor auth provider ' . $provider->getService(), 0, $exc);
 			}
 		}
 
