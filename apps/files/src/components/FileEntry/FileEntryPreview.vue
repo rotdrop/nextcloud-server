50c0e15112927507aea467cf8c01aee85ce31a08
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-05-13 12:23:23 +0200

    FileEntryPreview: hack in order to mount cafevdb mount points.
    In the progress of moving to Vue the files-app lost its ability to
    display custom folder icons. The indicators are now hard-coded and not
    stylable. So just hard-code an encryption "key" symbol for our special
    mount-points.

--- apps/files/src/components/FileEntry/FileEntryPreview.vue
+++ apps/files/src/components/FileEntry/FileEntryPreview.vue
@@ -208,6 +208,8 @@ export default defineComponent({
 					return CollectivesIcon
 				case 'shared':
 					return AccountPlusIcon
+				case 'cafevdb-database':
+					return KeyIcon
 			}
 
 			return null
