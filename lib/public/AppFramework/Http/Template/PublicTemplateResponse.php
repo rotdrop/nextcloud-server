0f826206c612399291af1dad2c078c9df068550c
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-10-09 16:48:49 +0200

    fix(l10n) load core translations in public template response.

--- lib/public/AppFramework/Http/Template/PublicTemplateResponse.php
+++ lib/public/AppFramework/Http/Template/PublicTemplateResponse.php
@@ -44,6 +44,7 @@ class PublicTemplateResponse extends TemplateResponse {
 		array $headers = [],
 	) {
 		parent::__construct($appName, $templateName, $params, 'public', $status, $headers);
+		\OCP\Util::addTranslations('core'); // these are needed
 		\OCP\Util::addScript('core', 'public-page-menu');
 		\OCP\Util::addScript('core', 'public-page-user-menu');
 
