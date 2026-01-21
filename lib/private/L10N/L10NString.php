d8d5e9b473ecc3e4b3f1eeb996a40b14421979bd
Claus-Justus Heine <himself@claus-justus-heine.de>
2026-01-21 20:09:49 +0100

    L10NString, convert backed enum parameters to their values.

--- lib/private/L10N/L10NString.php
+++ lib/private/L10N/L10NString.php
@@ -7,6 +7,8 @@
  */
 namespace OC\L10N;
 
+use BackedEnum;
+
 class L10NString implements \JsonSerializable {
 	/** @var L10N */
 	protected $l10n;
@@ -61,7 +63,7 @@ class L10NString implements \JsonSerializable {
 		// $count as %count% as per \Symfony\Contracts\Translation\TranslatorInterface
 		$text = $identityTranslator->trans($identity, $parameters);
 
-		return vsprintf($text, $this->parameters);
+		return vsprintf($text, array_map(fn(mixed $arg) => ($arg instanceof BackedEnum) ? $arg->value : $arg, $this->parameters));
 	}
 
 	public function jsonSerialize(): string {
